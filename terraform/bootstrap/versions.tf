07c10e872f6ef563c7ef5292d09117cd67c79541
N3N <narenarya@live.com>
2025-06-29 18:08:26 -0700

    feat: migrate local bootstrap state to remote backend

--- terraform/bootstrap/versions.tf
+++ terraform/bootstrap/versions.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # Local TF state is migrated to the current backend using `terraform init -migrate-state=false`
+  backend "s3" {
+    bucket         = "eskimo-tf-state"
+    key            = "bootstrap/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "eskimo-bootstrap-tf-lock"
+  }
 }
 
 provider "aws" {
